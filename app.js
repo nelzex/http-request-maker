3a99f243288773d91630bc65bbcea15680cefa24
Nelson Rowley <nelson@nelsons-mbp.home>
2020-05-03 16:40:02 +0100

    minor fixes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const ejs = require('ejs');
 const PORT = process.env.PORT || 5000;
 
 app.set('view engine','ejs');
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use('/', require('./routes/index'));
 
 
